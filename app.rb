4b341f3934501886a917070c0ea673fcff2c884a
Jonathan Cheatham <jcheatham@zendesk.com>
2015-01-05 21:33:57 -0800

    Adds support for displaying multiple projects simultaneously

--- app.rb
+++ app.rb
@@ -12,13 +12,15 @@ end
 
 TTL_PROJECTS = 60*60
 TTL_ERRORS = 24*60*60
-ERROR_REFRESH = 30
+REFRESH_LIMIT = 30
+DEFAULT_TIME = Time.at(0)
 
 set :logging, true
 set :dump_errors, true
 set :raise_errors, true
 
 use Rack::SSL if production?
+use Rack::Deflater
 use Rack::Session::Cookie, :secret => ENV["COOKIE_SECRET"]
 use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_OAUTH_CLIENT_ID"], ENV["GOOGLE_OAUTH_CLIENT_SECRET"], {:name => "google", :scope => "email"}
@@ -36,73 +38,78 @@ post '/auth/:provider/callback' do
   authorize!(omniauth_domain(request.env['omniauth.auth']))
 end
 
-get '/auth/failure' do
-  "Sorry pal, this stuff is restricted."
+get '/fail' do
+  "fail"
 end
 
 get '/' do
-  erb :index, :locals => {:projects => projects, :account => project_settings[session[:authorized_domain]][0]}
+  erb :index, :locals => {:projects => projects, :account => project_settings[session[:authorized_domain]][0], :selected_projects => Array(params[:projects])}
 end
 
-get '/errors/:project.json' do
+get '/flush' do
+  store.flush_all.to_s
+end
+
+get '/errors.json' do
   now = Time.now
-  begin
-    last_refresh, last_error, errors = store.get(cache_key) || [Time.at(0), Time.at(0), {}]
-  rescue Exception => e
-    puts "Ignoring #{e}, cache probably poisoned, will just refetch everything"
+  project_ids = projects.map(&:id) & params[:projects]
+
+  Parallel.each(project_ids, :in_threads => 10) do |project_id|
+    next unless (data[:last_refresh][project_id] + REFRESH_LIMIT) < now
+    data[:last_refresh][project_id] = now
+    most_recent_update = project_update = data[:last_update][project_id]
+    Parallel.each(Array(airbrake_errors(project_id)).compact, :in_threads => 10) do |error|
+      next unless error[:most_recent_notice_at] > project_update
+      most_recent_update = [error[:most_recent_notice_at], most_recent_update].max
+      error.delete(:updated_at) # airbrake is sending us '0001-01-01 00:00:00 UTC' which causes a marshal error
+      error[:id] = error[:id].to_s # convert IDs to string or risk javascript number precision errors
+      error[:project_id] = error[:project_id].to_s
+      error[:notices] = Array(airbrake_error_notices(error[:id])).compact
+      error[:notices].each{|n| n[:id] = n[:id].to_s ; n.delete(:project_id) }
+      data[:errors].merge!(error[:id] => error) do |key,oldval,newval|
+        newval[:notices].concat(oldval[:notices]).sort_by!{|a| a[:created_at] }.reverse!.uniq!{|b| b[:id] }
+        newval[:notices].slice!(30, newval[:notices].length)
+        newval
+      end
+    end
+    data[:last_update][project_id] = most_recent_update
   end
 
-  # safety net in case the cache returns nils
-  last_refresh ||= Time.at(0)
-  last_error   ||= Time.at(0)
-  errors       ||= {}
-
-  puts "#{now} - #{last_refresh} = #{now - last_refresh}"
-  current_errors = if ((now - last_refresh) > ERROR_REFRESH)
-    current_errors = merge(errors, recent_error_notices(last_error))
-    last_error = current_errors.values.max_by(&:most_recent_notice_at).most_recent_notice_at
-    puts "Storing #{current_errors.count} errors with last_error at #{last_error}"
-    store.set(cache_key, [now, last_error, current_errors], TTL_ERRORS)
-    current_errors
-  else
-    errors
-  end
-
-  JSON.dump current_errors
-end
-
-def cache_key
-  @cache_key ||= "air_monitor.#{request.path}.#{session[:authorized_domain]}"
-end
-
-def merge(error_notices, new_error_notices)
-  # NOTE TO SELF, this block is only evaluated when two keys collide and need a merge resolution
-  error_notices.merge!(new_error_notices) do |key,oldval,newval|
-    newval[:notices] = Array(newval[:notices])
-    newval[:notices].concat(oldval[:notices]).sort_by!{|a| a[:created_at] }.reverse!.uniq!{|b| b[:id] }
-    newval[:notices].slice!(30, newval[:notices].length)
-    newval
-  end
-  cutoff = Time.now - 12*60*60
-  error_notices.select do |k,v|
-    v[:notices].select!{|n| n.created_at > cutoff }
-    v[:frequency] = frequency(v[:notices]) || rough_frequency(v)
-    v[:notices].length > 0 || v[:frequency] > 1
-  end
-end
-
-def frequency(notices)
-  return if notices.length < 2
+  # cull old/inconsequential errors prior to saving to cache
   w = 0.5
-  period = notices.each_cons(2).map{|a,b| a[:created_at] - b[:created_at] }.reverse.reduce(nil){|p,d| w*d + (1-w)*(p||d)}
-  period = w * ([Time.now, notices.map{|n|n[:created_at]}.max].max - notices.first[:created_at]) + (1-w) * period
-  3600.0/period
+  w1 = 1 - w
+  data[:errors].select! do |error_id,error|
+    error[:frequency] = if error[:notices].length < 2
+      (error[:total_notices] || 1) / [error[:most_recent_notice_at] - error[:created_at], 1.0].max
+    else
+      events = error[:notices].map{|notice| notice[:created_at] }.sort
+      range_average = ([now, events.last].max - events.first)/events.count
+      weighted_interval_average = events.each_cons(2).map{|a,b| b - a }.reduce(nil){|p,d| w * d + w1 * (p||d)}
+      3600.0 / (w * range_average + w1 * weighted_interval_average)
+    end
+    error[:frequency] > 1.0
+  end
+
+  cache_data!
+
+  # cull errors belonging to projects that weren't requested
+  data[:errors].select! do |error_id,error|
+    project_ids.include?(error[:project_id])
+  end
+
+  JSON.dump data[:errors]
 end
 
-def rough_frequency(error)
-  range = error[:most_recent_notice_at] - error[:created_at]
-  range = 1 if range < 1
-  (error[:total_notices] || 1) / range
+
+
+def omniauth_domain(payload)
+  payload && payload[:info] && payload[:info][:email].to_s.split("@",2)[1].to_s.downcase
+end
+
+def authorize!(domain)
+  redirect '/fail' unless project_settings[domain]
+  session[:authorized_domain] = domain
+  redirect '/'
 end
 
 def airbrake
@@ -112,8 +119,24 @@ def airbrake
   end
 end
 
+def airbrake_errors(project_id)
+  airbrake.errors(:page => 1, :project_id => project_id)
+rescue Faraday::Error::ParsingError => e
+  puts "Bad response for http://#{airbrake.account}.airbrake.io/projects/#{project_id} - #{e}"
+rescue Exception => e
+  puts "Ignoring exception #{e} for #{project_id}"
+end
+
+def airbrake_error_notices(error_id)
+  airbrake.notices(error_id, :pages => 1, :raw => true)
+rescue Faraday::Error::ParsingError => e
+  puts "Bad response for http://#{airbrake.account}.airbrake.io/errors/#{error_id} - #{e}"
+rescue Exception => e
+  puts "Ignoring exception #{e} for #{error_id}"
+end
+
 def projects
-  store.fetch(cache_key, TTL_PROJECTS) do
+  store.fetch("air_monitor.projects.#{airbrake.account}", TTL_PROJECTS) do
     airbrake.projects
   end
 end
@@ -126,31 +149,20 @@ def store
   @store ||= Dalli::Client.new
 end
 
-def omniauth_domain(payload)
-  payload && payload[:info] && payload[:info][:email].to_s.split("@",2)[1].to_s.downcase
+def cache_data!
+  store.set("air_monitor.data.#{airbrake.account}", data, TTL_ERRORS)
 end
 
-def authorize!(domain)
-  redirect '/auth/failure' unless project_settings[domain]
-  session[:authorized_domain] = domain
-  redirect '/'
-end
-
-def recent_error_notices(since)
-  errors = airbrake.errors(:page => 1, :project_id => params[:project]) || []
-  puts "Selecting errors since #{since}"
-  errors.select!{|e| e[:most_recent_notice_at] > since }
-  Parallel.map(errors, :in_threads => 10) do |error|
-    begin
-      error.delete(:updated_at) # for some reason we're getting '0001-01-01 00:00:00 UTC' in here which causes a marshal error
-      error[:id] = error[:id].to_s
-      error[:notices] = airbrake.notices(error[:id], :pages => 1, :raw => true).compact
-      error[:notices].each{|n| n[:id] = n[:id].to_s ; n[:project_id] = n[:project_id].to_s }
-    rescue Faraday::Error::ParsingError => e
-      puts "Ignoring notices for #{error}, got 500 from http://#{airbrake.account}.airbrake.io/errors/#{error[:id]}"
+def data
+  @data ||= begin
+    cached_data = begin
+      store.get("air_monitor.data.#{airbrake.account}")
     rescue Exception => e
-      puts "Ignoring exception #{e}"
-    end
-    error
-  end.compact.each_with_object({}){|e,h|h[e.id]=e}
+      puts "Ignoring #{e}, cache probably poisoned, will just refetch everything"
+    end || {}
+    cached_data[:last_refresh] ||= Hash.new(DEFAULT_TIME)
+    cached_data[:last_update] ||= Hash.new(DEFAULT_TIME)
+    cached_data[:errors] ||= {}
+    cached_data
+  end
 end
